5ce0c7f191328a8475a5411099f327ce9f7542f7
Mark <mark.scrudder@gmail.com>
2021-05-10 13:26:08 -0700

    Removed comment related to correction already made.

--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -4,7 +4,6 @@ const Entry = ({ entry, time }) => {
 
   // TODO
   // Make it prettier
-  // Cut irrelevent data from date to just show timestamp - this may be done on Daily page.
 
   return (
     <div className='entry'>
